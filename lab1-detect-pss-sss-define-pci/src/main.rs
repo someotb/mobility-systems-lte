8b8eeb1f4079260fe56a9ebb98b57eb0ce523437
someotb <llyryk934@gmail.com>
2026-09-11 23:52:38 +0700

    chore: add TODO

--- lab1-detect-pss-sss-define-pci/src/main.rs
+++ lab1-detect-pss-sss-define-pci/src/main.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+// TODO: Generate PSS, correlate with PSS from .pcm
+
 fn main() {
     let samples = utils::read_file("data/lte_pss_sss_pbch_iq_s16le.pcm");
     println!("{:?}", samples);
