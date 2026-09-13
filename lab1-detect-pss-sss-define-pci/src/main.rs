c79527cce14912ffa547208a3fbb0f2ba903619d
someotb <llyryk934@gmail.com>
2026-09-13 10:03:09 +0700

    feat: generate PSS

--- lab1-detect-pss-sss-define-pci/src/main.rs
+++ lab1-detect-pss-sss-define-pci/src/main.rs
@@ -1,8 +1,10 @@
 mod utils;
 
-// TODO: Generate PSS, correlate with PSS from .pcm
+// TODO: apply IFFT to PSS
 
 fn main() {
     let samples = utils::read_file("data/lte_pss_sss_pbch_iq_s16le.pcm");
-    println!("{:?}", samples);
+    println!("Samples: \n{:#?}", samples);
+    let pss = utils::generate_pss(utils::Nid2::Two);
+    println!("PSS: \n{:#?}", pss)
 }
--- lab1-detect-pss-sss-define-pci/src/utils.rs
+++ lab1-detect-pss-sss-define-pci/src/utils.rs
@@ -1,10 +1,41 @@
+use num_complex::Complex64;
 use std::fs;
 
-pub fn read_file(filename: &str) -> Vec<i16> {
+pub enum Nid2 {
+    Zero,
+    One,
+    Two,
+}
+
+pub fn read_file(filename: &str) -> Vec<Complex64> {
     let bytes = fs::read(filename).unwrap();
-    let mut samples = Vec::new();
-    for chunk in bytes.chunks(2) {
-        samples.push(i16::from_le_bytes([chunk[0], chunk[1]]));
+    let mut samples: Vec<Complex64> = Vec::new();
+    for chunk in bytes.chunks(4) {
+        let i = i16::from_le_bytes([bytes[0], bytes[1]]) as f64;
+        let q = i16::from_le_bytes([bytes[2], bytes[3]]) as f64;
+        samples.push(Complex64::new(i, q));
     }
     samples
+}
+
+pub fn generate_pss(nid: Nid2) -> Vec<Complex64> {
+    let root_index: [i32; 3] = [25, 29, 34];
+    let mut phase: f64;
+    let mut d_u: Vec<Complex64> = Vec::new();
+    let idx = match nid {
+        Nid2::Zero => 0,
+        Nid2::One => 1,
+        Nid2::Two => 2,
+    };
+    for n in 0..=61 {
+        let u = root_index[idx];
+        if n <= 30 {
+            phase = -std::f64::consts::PI * (u as f64) * (n as f64) * ((n + 1) as f64) / 63.0;
+        } else {
+            phase = -std::f64::consts::PI * (u as f64) * ((n + 1) as f64) * ((n + 2) as f64) / 63.0;
+        }
+        let d = Complex64::new(0.0, phase).exp();
+        d_u.push(d);
+    }
+    d_u
 }
